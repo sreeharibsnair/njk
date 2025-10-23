ad28f5bd70acedd8f52fc8484a3d2855f9900ed4
Sreehari Nair <sreeharibsnair04@gmail.com>
2025-10-23 11:03:07 +0530

    py

---
+++ EXP14Q2/GRAPHICS/GRAPHICS3D/cuboid.py
@@ -0,0 +1,5 @@
+def cuboidarea(l, b, h):
+    print("Total Surface Area of Cuboid:", 2 * ((l * b) + (l * h) + (b * h)))
+
+def cuboidperi(l, b, h):
+    print("Perimeter of Cuboid:", 4 * (l + b + h))
---
+++ EXP14Q2/GRAPHICS/GRAPHICS3D/sphere.py
@@ -0,0 +1,7 @@
+import math
+
+def spherearea(r):
+    print("Surface Area of Sphere:", 4 * math.pi * r * r)
+
+def sphereperi(r):
+    print("Circumference of Sphere:", 2 * math.pi * r)
